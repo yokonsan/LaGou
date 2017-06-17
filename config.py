5a9488de07b947b130ea17b6c3e8e6e36bcda5f2
yukun <15152347277@163.com>
2017-06-17 17:47:37 +0800

    一些修改

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 MONGO_URL = 'localhost'
+# 数据库名
 MONGO_DB = 'lagou'
 
+# 表名
 MONGO_TABLE = 'Python'
 
-USERNAME = '...'
-PASSWORD = '...'
+# 拉勾网用户名和密码
+USERNAME = '123'
+PASSWORD = 'abc'
--- indexspider.py
+++ indexspider.py
@@ -2,13 +2,19 @@
 #  author: yukun
 import requests
 from bs4 import BeautifulSoup
+from requests.exceptions import RequestException
 
 
 def get_html(url):
 	headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
-	resp = requests.get(url, headers=headers).text
-
-	return resp
+	try:
+		resp = requests.get(url, headers=headers)
+		if resp.status_code == 200:
+			print('ok')
+			return resp.text
+		return None
+	except RequestException:
+		return None
 
 def parse_index():
 	url = 'https://www.lagou.com/'
@@ -24,7 +30,4 @@ def parse_index():
 			'name' : jobname
 		}
 		datas.append(data)
-	print(datas)
 	return datas
-
-parse_index()
